b6a37295c72f28a89fd9842573239a237c2369de
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-22 13:10:21 +0100

    clean up angle wrapper

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM dynverse/dynwrap:r
 
 ADD . /code
 
-LABEL version 0.1.4
+LABEL version 0.1.5
 
 ENTRYPOINT Rscript /code/run.R
--- run.R
+++ run.R
@@ -4,45 +4,45 @@ library(dplyr)
 library(purrr)
 library(dyndimred)
 
-#   ____________________________________________________________________________
-#   Load data                                                               ####
+#####################################
+###           LOAD DATA           ###
+#####################################
 
 data <- read_rds("/ti/input/data.rds")
 params <- jsonlite::read_json("/ti/input/params.json")
 
-#' @examples
-#' data <- dyntoy::generate_dataset(model = "cyclic") %>% c(., .$prior_information)
-#' params <- yaml::read_yaml("containers/angle/definition.yml")$parameters %>%
-#'   {.[names(.) != "forbidden"]} %>%
-#'   map(~ .$default)
+#####################################
+###        INFER TRAJECTORY       ###
+#####################################
 
-#   ____________________________________________________________________________
-#   Infer trajectory                                                        ####
+# use seed if provided
+if (!is.null(params$seed) && !is.na(params$seed)) set.seed(params$seed)
 
+# fetch expression data
 expression <- data$expression
 
 # TIMING: done with preproc
-checkpoints <- list(method_afterpreproc = as.numeric(Sys.time()))
+timings <- list(method_afterpreproc = Sys.time())
 
 # perform PCA dimred
-space <- dyndimred::dimred(expression, method = params$dimred, ndim = 2)
+dimred <- dyndimred::dimred(expression, method = params$dimred, ndim = 2)
 
 # transform to pseudotime using atan2
-pseudotime <- atan2(space[,2], space[,1]) / 2 / pi + .5
+pseudotime <- atan2(dimred[,2], dimred[,1]) / 2 / pi + .5
 
 # TIMING: done with method
-checkpoints$method_aftermethod <- as.numeric(Sys.time())
+timings$method_aftermethod <- Sys.time()
 
 # return output
 output <- lst(
   cell_ids = rownames(expression),
-  pseudotime = pseudotime,
+  pseudotime,
   do_scale_minmax = FALSE,
-  dimred = space,
-  timings = checkpoints
+  dimred,
+  timings
 )
 
-#   ____________________________________________________________________________
-#   Save output                                                             ####
-
+#####################################
+###     SAVE OUTPUT TRAJECTORY    ###
+#####################################
 write_rds(output, "/ti/output/output.rds")
